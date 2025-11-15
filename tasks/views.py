b0ef1230ad52e62e1687eb7de2119104b40fa933
Md Redwan Islam <redwanislamsiam13@gmail.com>
2025-11-15 19:31:58 +0600

    Initial Project Done

---
+++ tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello World")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+
+def show_task(request):
+    return HttpResponse("This is Show-task")
